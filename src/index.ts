61dd0dbf1f6250b6bbbe43a40f91779390444f44
FlorentinoG9 <f.valle.garcia@gmail.com>
2025-11-10 19:41:05 -0600

    Import CSS styles and reorganize exports in index.ts for improved structure and clarity.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-export { PMTilesMap, default } from './pmtiles-map';
-export type { PMTilesMapProps, PMTilesSourceOptions, BasemapFlavor, MarkerPin } from './types';
-export { initializePMTilesProtocol, createProtomapsStyle, createDefaultOSMStyle } from './utils';
-export { createMarker, createDefaultMarkerIcon, createMarkerIcon } from './marker-utils';
+// Import CSS styles - these will be included automatically when the package is imported
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './styles.css';
+
+export { createDefaultMarkerIcon, createMarker, createMarkerIcon } from './marker-utils';
+export { default, PMTilesMap } from './pmtiles-map';
+export type { BasemapFlavor, MarkerPin, PMTilesMapProps, PMTilesSourceOptions } from './types';
+export { createDefaultOSMStyle, createProtomapsStyle, initializePMTilesProtocol } from './utils';
 
